a81dd5b7d9f559894c7dcaa126241b4b3fdd2bd7
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-04-20 09:56:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+//your code here
+function countChar(countFrom,displayout){
+	var length = document.getElementById(countFrom).value.length;
+	document.getElementById(displayout).innerHTML = length;
+}
